371e8d2540acc1238401f641e43d55059a24b96b
May <maykhine.ada@gmail.com>
2023-10-12 19:00:44 -0400

    add toggle function

--- Section13_Proj/src/App.js
+++ Section13_Proj/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react"
+import React, { useCallback, useState } from "react"
 
-import Button from "./components/UI/Button"
-
+import Button from "./components/UI/Button/Button"
+import DemoOutput from "./components/Demo/DemoOutput"
+// import DemoOutput from "./components/Demo/DemoOutput"
 import "./App.css"
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false)
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) setShowParagraph(!showParagraph)
+  }, [allowToggle])
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
-      <Button></Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={setAllowToggle}>Allow Toggle</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   )
 }
---
+++ Section13_Proj/src/components/Demo/Paragraph.js
@@ -0,0 +1,5 @@
+export const Paragraph = (props) => {
+  console.log("MyParagraph is running")
+
+  return <p>{props.children}</p>
+}
